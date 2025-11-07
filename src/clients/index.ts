f8ce238db0b4123a2dca83a66b31ba2fc28d36a7
Alex Fernandes <contato.alexnicolas@gmail.com>
2025-11-07 14:40:31 -0300

    feat: update accountId uuid

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const apiClient = axios.create({
     baseURL: 'http://localhost:8080',
     headers: {
-        'accountId': '9e34455a-5d49-4c8f-9207-2edec49af3d9',
+        'accountId': '3211b261-fd8b-4987-abf8-2e85a293c265',
     }
 });
